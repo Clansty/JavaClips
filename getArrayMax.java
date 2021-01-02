b54697f5bf6e97eee7d308a549d6b3335c857089
凌莞~(=^▽^=) <i@gao4.pw>
2021-01-02 10:07:17 +0800

    Create getArrayMax.java

---
+++ getArrayMax.java
@@ -0,0 +1,8 @@
+    public static int getArrayMax(int[] arr) {
+        int max = arr[0];
+        for (int i : arr) {
+            if (i > max)
+                max = i;
+        }
+        return max;
+    }
